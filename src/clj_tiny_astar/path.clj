e9c090503c3ebcad1a5354bf28da37ffd9fc78f9
danstone <dan.stone16312@gmail.com>
2013-11-06 22:19:51 +0000

    can now pass distance heuristic to pather

--- src/clj_tiny_astar/path.clj
+++ src/clj_tiny_astar/path.clj
@@ -76,14 +76,14 @@
 (defn f [square] (+ (:g square) (:h square)))
 
 (defn a*-adj-squares
-  [pred closed loc goal]
+  [dist pred closed loc goal]
   (->> (adj loc)
        (filter #(and (pred %)
                      (not (closed %))))
        (map (fn [p]
               (->Square p
                         (if (diagonal? loc p) 1.4 1)
-                        (manhattan-dist p goal)
+                        (dist p goal)
                         loc)))))
 
 (defn a*-collect-path
@@ -94,31 +94,35 @@
       (recur (conj acc (:loc parent)) parent)
       acc)))
 
+
+
 (defn a*
 "find a path from a to b given the predicate function 'pred'.
 pred is a function of point -> bool"
-  [pred a b]
+([dist pred a b]
   (loop [open (initial-open-set b)
-         squares {b (->Square b 0 0 nil)}
-         closed #{}]
-    (if-not (empty? open)
-      (let [curr (first-first open)
-            curr-square (squares curr)]      
-        (if (= curr a)
-          (a*-collect-path curr-square squares)
-          (let [adj (a*-adj-squares pred closed curr a)             
-                reducer (fn [[squares open :as data] a]
-                          (let [loc (:loc a)]
-                            (cond
-                             (not (open loc))
-                             (tuple (assoc squares loc a)
-                                    (assoc open loc (f a)))
-                             (< (:g a) (:g (squares loc)))
-                             (tuple (assoc squares loc a)
-                                    open)
-                             :else data)))
-                [new-squares new-open] (reduce reducer (tuple squares open) adj)]
-            (recur (dissoc new-open curr) new-squares (conj closed curr))))))))
+          squares {b (->Square b 0 0 nil)}
+          closed #{}]
+     (if-not (empty? open)
+       (let [curr (first-first open)
+             curr-square (squares curr)]      
+         (if (= curr a)
+           (a*-collect-path curr-square squares)
+           (let [adj (a*-adj-squares dist pred closed curr a)             
+                 reducer (fn [[squares open :as data] a]
+                           (let [loc (:loc a)]
+                             (cond
+                              (not (open loc))
+                              (tuple (assoc squares loc a)
+                                     (assoc open loc (f a)))
+                              (< (:g a) (:g (squares loc)))
+                              (tuple (assoc squares loc a)
+                                     open)
+                              :else data)))
+                 [new-squares new-open] (reduce reducer (tuple squares open) adj)]
+             (recur (dissoc new-open curr) new-squares (conj closed curr))))))))
+([pred a b]
+   (a* manhattan-dist pred a b)))
 
 
 
